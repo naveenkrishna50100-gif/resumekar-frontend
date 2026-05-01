e0b30f92781cb37fa4075c06e87e66d716361912
naveenkrishna50100-gif <naveenkrishna50100@gmail.com>
2026-05-01 21:57:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Dashboard from './pages/Dashboard';
 import Evaluate from './pages/Evaluate';
 import Profile from './pages/Profile';
 import Pricing from './pages/Pricing';
+import AuthCallback from './pages/AuthCallback';
 
 function ProtectedRoute({ children }) {
   const { user, loading } = useAuth();
@@ -33,6 +34,7 @@ function AppRoutes() {
       <Route path="/" element={<PublicRoute><Landing /></PublicRoute>} />
       <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
       <Route path="/signup" element={<PublicRoute><Signup /></PublicRoute>} />
+      <Route path="/auth/callback" element={<AuthCallback />} />
       <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
       <Route path="/evaluate" element={<ProtectedRoute><Evaluate /></ProtectedRoute>} />
       <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
