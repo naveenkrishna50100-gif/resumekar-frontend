28fc8672e4065991342ab7322cf68b6787bb8566
naveenkrishna50100-gif <naveenkrishna50100@gmail.com>
2026-05-07 22:06:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-if (window.location.hash && window.location.hash.includes('access_token')) {
-  window.location.replace('/auth/callback' + window.location.hash);
-}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
